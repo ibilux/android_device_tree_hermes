319a4e97a649448d84963a5fb88dad2bcd69e4a8
ibilux <i.bilux@gmail.com>
2018-12-29 15:19:12 +0100

    lineage.mk: rebranding for cm -> lineage

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from hermes device
 $(call inherit-product, device/xiaomi/hermes/device.mk)
