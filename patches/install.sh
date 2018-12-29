6bd233bc99be7ace64c25bfb9f57b1ae1a63de57
ibilux <i.bilux@gmail.com>
2018-12-29 17:46:29 +0100

    patches: update patches for lineage-15.1

--- patches/install.sh
+++ patches/install.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
+
+echo $1
 rootdirectory="$PWD"
-dirs="frameworks/av frameworks/base frameworks/native system/core system/media system/netd system/sepolicy packages/apps/FMRadio prebuilts/sdk"
+# ---------------------------------
+
+dirs="build/make/core build/soong hardware/interfaces frameworks/av frameworks/base frameworks/native system/core system/media system/netd system/sepolicy packages/apps/FMRadio prebuilts/sdk"
+
+# red + nocolor
+RED='\033[0;31m'
+NC='\033[0m'
+
 
 for dir in $dirs ; do
 	cd $rootdirectory
 	cd $dir
-    echo "Applying $dir patches..."
-	git apply $rootdirectory/device/xiaomi/hermes/patches/$dir/*.patch
+	echo -e "\n${RED}Applying ${NC}$dir ${RED}patches...${NC}\n"
+	#git am $rootdirectory/device/xiaomi/hermes/patches/$dir/*.patch
+	git apply -v $rootdirectory/device/xiaomi/hermes/patches/$dir/*.patch
 done
 
-echo "Done!"
+# -----------------------------------
+echo -e "Done !\n"
 cd $rootdirectory
