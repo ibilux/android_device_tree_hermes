51cd3def8be7725b858c2bbe1daf8dc8a0a57f85
ibilux <i.bilux@gmail.com>
2018-12-29 15:06:55 +0100

    just re-organize some codes for better reading

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-#Misc
+# Build old-style zip files (required for ota updater)
 BLOCK_BASED_OTA := false
 
 # Kernel
