3b24997970d1544a3f27de19d3a2c6b799d8e70a
ibilux <i.bilux@gmail.com>
2018-12-30 05:33:22 +0100

    overlay-lineage: Apply LineageOS rebrand (move Lineage specific overlay to lineage-overlay)

--- device.mk
+++ device.mk
@@ -12,7 +12,9 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 include $(LOCAL_PATH)/props.mk
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+         $(LOCAL_PATH)/overlay \
+         $(LOCAL_PATH)/overlay-lineage
 
 # Display
 TARGET_SCREEN_HEIGHT := 1920
