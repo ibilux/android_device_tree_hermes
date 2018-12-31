fb4bbe2d5bd72c4b6af284c4b0f926f5ceff3a8d
ibilux <i.bilux@gmail.com>
2018-12-31 12:53:53 +0100

    props.mk: update PRODUCT_DEFAULT_PROPERTY_OVERRIDES and PRODUCT_PROPERTY_OVERRIDES

--- props.mk
+++ props.mk
@@ -1,3 +1,19 @@
+### product default property overrides ###
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.timezone=Europe/Moscow \
+    ro.allow.mock.location=0 \
+    ro.zygote=zygote64_32 \
+    ro.dalvik.vm.native.bridge=0 \
+    persist.sys.usb.config=mtp \
+    persist.debug.xlog.enable=0
+
+### product property overrides ###
+
+# Enable call recording
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.call_recording.enabled=true \
+    persist.call_recording.src=4
+
 # Storage
 PRODUCT_PROPERTY_OVERRIDES += ro.sys.sdcardfs=false
 
@@ -5,12 +21,3 @@ PRODUCT_PROPERTY_OVERRIDES += ro.sys.sdcardfs=false
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.usb.config=adb
 endif
-
-# Timezone
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-     persist.sys.timezone=Europe/Moscow
-
-# Enable call recording
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.call_recording.enabled=true \
-    persist.call_recording.src=4
