1664482ab56aa50ce700f07dafb4384303f318be
ibilux <i.bilux@gmail.com>
2018-12-31 11:18:08 +0100

    update ext4fs and add Filesystem management tools

--- device.mk
+++ device.mk
@@ -177,3 +177,29 @@ PRODUCT_COPY_FILES += \
 
 # include from product/*.mk
 -include $(LOCAL_PATH)/product/*.mk
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	e2fsck \
+	fibmap.f2fs \
+	fsck.f2fs \
+	mkfs.f2fs \
+	make_ext4fs \
+	resize2fs \
+	setup_fs \
+	ext4_resize \
+	libext2_blkid \
+	libext2_uuid_static \
+	superumount
+
+# exFAT
+PRODUCT_PACKAGES += \
+	mount.exfat \
+	fsck.exfat \
+	mkfs.exfat
+
+# NTFS
+PRODUCT_PACKAGES += \
+	fsck.ntfs \
+	mkfs.ntfs \
+	mount.ntfs
