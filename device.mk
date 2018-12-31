6d49f75c084664efe5381b173e1fc9f1a8d49ef4
ibilux <i.bilux@gmail.com>
2018-12-31 12:53:53 +0100

    device.mk: update PRODUCT_DEFAULT_PROPERTY_OVERRIDES and PRODUCT_PROPERTY_OVERRIDES

--- device.mk
+++ device.mk
@@ -189,9 +189,16 @@ PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
 #    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-     persist.sys.timezone=Europe/Moscow
+# product default property overrides
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.timezone=Europe/Moscow \
+    ro.allow.mock.location=0 \
+    ro.zygote=zygote64_32 \
+    ro.dalvik.vm.native.bridge=0 \
+    persist.sys.usb.config=mtp \
+    persist.debug.xlog.enable=0
 
+# product property overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.call_recording.enabled=true \
     persist.call_recording.src=4
