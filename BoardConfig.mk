f59d9940459f300c4dd2c4592684a9d327489fa7
bilux <i.bilux@gmail.com>
2019-01-23 18:05:11 +0100

    BoardConfig.mk: we get the official kernel source
    Now that we have the official kernel source:
    - We don't need to revert sepolicy < 30 ;
    - We don't need to minijail patch for seccomp ;

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,11 +112,11 @@ BOARD_SUPPRESS_EMMC_WIPE := true
 BOARD_RECOVERY_SWIPE := true
 
 # Seccomp
-TARGET_DOES_NOT_SUPPORT_SECCOMP := true
+#TARGET_DOES_NOT_SUPPORT_SECCOMP := true
 
 # Sepolicy
 BOARD_SEPOLICY_DIRS += device/xiaomi/hermes/sepolicy
-POLICYVERS := 28
+#POLICYVERS := 28
 
 # Build kernel without kernel sources
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
