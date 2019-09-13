d90c10d878f9bad353ae05238bfd9beed4ca1a73
bilux <i.bilux@gmail.com>
2019-09-13 12:06:58 +0100

    libshims: Shim library for missing symbols in libui
    Signed-off-by: bilux <i.bilux@gmail.com>

--- mtk/libshims/Android.mk
+++ mtk/libshims/Android.mk
@@ -26,3 +26,12 @@ LOCAL_MODULE := libshim_netutils
 LOCAL_CFLAGS := -Wno-unused-variable -Wno-unused-parameter
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+# libshim_ui
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libui.cpp
+LOCAL_SHARED_LIBRARIES := libbinder libui libutils
+LOCAL_MODULE := libshim_ui
+LOCAL_CFLAGS := -Wno-unused-variable -Wno-unused-parameter
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
