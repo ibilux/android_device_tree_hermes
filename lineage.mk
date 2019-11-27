0e09f2b6ac959c7197a717517e9391e4579c8d48
bilux <i.bilux@gmail.com>
2019-11-27 12:06:08 +0100

    lineage.mk: set the fingerprint for all variant
    we are still in the userdebug variant, so let's override it for now.
    
    Signed-off-by: bilux <i.bilux@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -31,8 +31,11 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 2
 PRODUCT_MANUFACTURER := Xiaomi
 
-ifeq ($(TARGET_BUILD_VARIANT),user)
+#ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 BUILD_FINGERPRINT := "Xiaomi/hermes/hermes:5.0.2/LRX22G/V9.6.3.0.LHMMIFD:user/release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="hermes-user 5.0.2 LRX22G V9.6.3.0.LHMMIFD release-keys"
-endif
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=hermes \
+    PRODUCT_NAME=lineage_hermes \
+    PRIVATE_BUILD_DESC="hermes-user 5.0.2 LRX22G V9.6.3.0.LHMMIFD release-keys"
+#endif
