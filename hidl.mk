121020fc2a96dfa2a76a6ce064f20eef9b04f217
bilux <i.bilux@gmail.com>
2020-01-03 22:55:20 +0100

    hidl: add vibrator hal
    Error was:
    "android.hardware.vibrator@1.0-service.mediatek: Failed to open /sys/kernel/thunderquake_engine/level (2): No such file or directory"
    
    Signed-off-by: bilux <i.bilux@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -69,7 +69,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.mediatek
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
