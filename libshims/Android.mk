b718db4cd27c538a182dd1393f9859a7b3acb544
bilux <i.bilux@gmail.com>
2020-01-04 09:45:17 +0100

    libshims: shim missing GraphicBuffer symbol
    Error was:
    HAL : dlopen failed: cannot locate symbol "_ZN7android13GraphicBufferC1Ejjij" referenced by "/system/vendor/lib/libcam_utils.so"...
    
    Signed-off-by: bilux <i.bilux@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -126,11 +126,14 @@ include $(BUILD_SHARED_LIBRARY)
 ## libshim_ui
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := ui/mtk_ui.cpp
+LOCAL_SRC_FILES := \
+	ui/mtk_ui.cpp \
+	ui/mtk_gbc1.cpp
 
 LOCAL_SHARED_LIBRARIES := \
 	libbinder \
-	libui
+	libui \
+	libutils
 
 LOCAL_MODULE := libshim_ui
 
