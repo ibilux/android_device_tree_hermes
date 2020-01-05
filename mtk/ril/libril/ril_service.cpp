77b7248837803dfbcdac3726bfd0827f4011f25d
bilux <i.bilux@gmail.com>
2020-01-05 16:08:03 +0100

    MTK: libril: initialize rild sequence
    Change rild initial sequence to guarantee non-null function pointer before rild register its hidl service
    
    Signed-off-by: bilux <i.bilux@gmail.com>

--- mtk/ril/libril/ril_service.cpp
+++ mtk/ril/libril/ril_service.cpp
@@ -8439,6 +8439,9 @@ void radio::registerService(RIL_RadioFunctions *callbacks, CommandInfo *commands
     simCount = SIM_COUNT;
     #endif
 
+    s_vendorFunctions = callbacks;
+    s_commands = commands;
+
     configureRpcThreadpool(1, true /* callerWillJoin */);
     for (int i = 0; i < simCount; i++) {
 	pthread_rwlock_t *radioServiceRwlockPtr = getRadioServiceRwlock(i);
@@ -8457,9 +8460,6 @@ void radio::registerService(RIL_RadioFunctions *callbacks, CommandInfo *commands
 	ret = pthread_rwlock_unlock(radioServiceRwlockPtr);
 	assert(ret == 0);
     }
-
-    s_vendorFunctions = callbacks;
-    s_commands = commands;
 }
 
 void rilc_thread_pool() {
