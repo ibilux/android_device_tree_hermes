14862376ef47558b6caced0bab2ba08637050e16
bilux <i.bilux@gmail.com>
2020-03-20 22:36:00 +0100

    manifest: update some hidl's
    Signed-off-by: bilux <i.bilux@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -1,112 +1,109 @@
-# Power (non-lineage imp)
-PRODUCT_PACKAGES += android.hardware.power@1.0-impl
-
-# Power (lineage imp)
-#PRODUCT_PACKAGES += vendor.lineage.power@1.0-impl
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
-# GNSS HAL
+# GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service \
 
 # GPS force mode
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.force.gps.mode=gnss
 
-# Audio HIDL
+# Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio.effect@4.0-impl \
 
-# gralloc/graphics HAL/HW Composer
+# FM Radio
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0-impl
+
+# Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.renderscript@1.0-impl
+    android.hardware.graphics.mapper@2.0-impl
 
 # memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
-# Camera HAL
+# Camera
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl-legacy
 
-# Bluetooth HAL
+# Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
-# USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+# USB
+PRODUCT_PACKAGES += android.hardware.usb@1.0-service.basic
 
 # Omx
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0
 
-# Keymaster HIDL
+# Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
-    #android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
-# Gatekeeper HIDL
+# Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl
-    #android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# ConsumerIr HIDL
+# ConsumerIr
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
-# Lights
+# Light
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
 # LiveDisplay
+ifneq ($(wildcard vendor/lineage/.),)
 PRODUCT_PACKAGES += vendor.lineage.livedisplay@2.0-service-mediatek
+endif
 
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
-# Drm
+# DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service.widevine \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-service.override
 
 # Ril
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0 \
     android.hardware.radio@1.1 \
     android.hardware.radio.deprecated@1.0
-    #vendor.mediatek.hardware.radio@1.1 \
-    #vendor.mediatek.hardware.radio@2.0 \
-    #vendor.mediatek.hardware.radio.deprecated@1.1
 
-# Tether Packages
-PRODUCT_PACKAGES += \
-    android.hardware.tetheroffload.config@1.0 \
-    android.hardware.tetheroffload.control@1.0
+# Configstore
+PRODUCT_PACKAGES += android.hardware.configstore@1.1-service
