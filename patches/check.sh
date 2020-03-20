2d398a0c9a6f872516a89a18193cfe86e13f25fb
bilux <i.bilux@gmail.com>
2020-03-21 00:06:45 +0100

    patches: update patches for #pie
    Signed-off-by: bilux <i.bilux@gmail.com>

--- patches/check.sh
+++ patches/check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="bionic build/make/core build/soong frameworks/av frameworks/base frameworks/native frameworks/opt/telephony hardware/interfaces hardware/ril packages/apps/Settings system/core system/sepolicy"
+dirs="frameworks/av frameworks/base frameworks/opt/telephony packages/apps/FMRadio system/core"
 
 RED='\033[0;31m'
 NC='\033[0m'
