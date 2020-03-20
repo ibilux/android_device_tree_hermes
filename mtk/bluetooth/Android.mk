49c4adb5630ca4368f016a5a83279616d48b66fe
bilux <i.bilux@gmail.com>
2020-03-20 21:44:55 +0100

    MTK: Bluetooth: update bt library
    Signed-off-by: bilux <i.bilux@gmail.com>

--- mtk/bluetooth/Android.mk
+++ mtk/bluetooth/Android.mk
@@ -1,1 +1,41 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+###########################################################################
+# MTK BT CHIP INIT LIBRARY FOR BLUEDROID
+###########################################################################
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+  mtk.c \
+  radiomgr.c \
+  radiomod.c
+
+LOCAL_C_INCLUDES := \
+  system/bt/hci/include \
+  $(LOCAL_PATH)/include
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libbluetooth_mtk
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := mtk
+LOCAL_SHARED_LIBRARIES := liblog libcutils libnvram
+LOCAL_PRELINK_MODULE := false
+include $(BUILD_SHARED_LIBRARY)
+
+###########################################################################
+# MTK BT DRIVER FOR BLUEDROID
+###########################################################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := bt_drv.c
+
+LOCAL_C_INCLUDES := \
+  system/bt/hci/include \
+  $(LOCAL_PATH)/include
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libbt-vendor
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := mtk
+LOCAL_SHARED_LIBRARIES := liblog libbluetooth_mtk
+LOCAL_PRELINK_MODULE := false
+include $(BUILD_SHARED_LIBRARY)
