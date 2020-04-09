6c9e8c36aa094e75392ea24577e9b82cd4a04153
bilux <i.bilux@gmail.com>
2020-04-09 19:03:05 +0100

    hidl: remove widevine hidl
    it caused high cpu load in surfaceflinger process
    
    Signed-off-by: bilux <i.bilux@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -91,8 +91,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.clearkey \
-    android.hardware.drm@1.0-service.widevine
 
 # Health HAL
 PRODUCT_PACKAGES += \
