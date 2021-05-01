3a0248b9a117ad1292f393b89671e972d1e2876e
bilux <i.bilux@gmail.com>
2021-05-02 00:24:23 +0100

    hidl: downgrade audio version to 2.0
    Signed-off-by: bilux <i.bilux@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -17,9 +17,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio HAL
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl
+    android.hardware.audio.effect@2.0-impl
 
 # FM Radio HAL
 PRODUCT_PACKAGES += \
